c4ca8f3d843e53fa21df245adeaa622c69136b40
okayleigh-coder <okayleighg@gmail.com>
2026-06-11 10:46:45 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+animation.runImageAnimation(
+mySprite,
+assets.animation`Butcher_RightIdle`,
+500,
+true
+)
+controller.moveSprite(mySprite, 100, 0)
